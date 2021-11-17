25bf81fb95303da65b3708674949028ef0d88b1a
Newman251 <79203167+Newman251@users.noreply.github.com>
2021-11-17 11:45:39 +0100

    Search bar and search button

---
+++ src/components/molecules/InputField/InputField.tsx
@@ -0,0 +1,57 @@
+
+import React from 'react';
+import styled from "styled-components/native";
+
+interface Props {
+  title: string;
+  onPress: () => void;
+}
+
+const StyledText  = styled.Text`
+    color: white;
+    font-size: 10px;
+    text-align: center;
+    vertical-align: middle;
+`;
+
+const StyledPressable = styled.Pressable`
+  background-color: blue;
+  border-radius: 50px;
+  line-height: 10px;
+  padding: 8px 20px;
+  vertical-align: middle;
+`
+
+const ButtonContainer = styled.TouchableOpacity`
+  margin-vertical: 40px;
+  width: 250px;
+  height: 40px;
+  padding: 12px;
+  border-radius: 10px;
+  background-color: #111121;
+`
+
+const InputField = (props: Props) => {
+  const {title, onPress} = props;
+  return (
+    <form className="input">
+      <StyledText>
+      <ButtonContainer>
+      <input
+        placeholder="Search by Keywords"
+      />
+      <input
+        placeholder="Search by Location"
+      />
+      </ButtonContainer>
+      </StyledText>
+      <StyledText>
+      <StyledPressable onPress={onPress}>
+        {title}
+      </StyledPressable>
+      </StyledText>
+    </form>
+  );
+};
+
+export default InputField;
