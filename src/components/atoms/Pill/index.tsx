db60b7f81ef0629c901621b6a813d4f684227c96
Newman251 <79203167+Newman251@users.noreply.github.com>
2021-11-17 13:04:36 +0100

    Platform selection pills

---
+++ src/components/atoms/Pill/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Button, Pressable, Text} from "react-native";
+import styled from "styled-components/native";
+
+
+interface Props {
+    title: string;
+    onPress: () => void;
+}
+
+const StyledText  = styled.Text`
+    color: white;
+    font-size: 17px;
+    text-align: center;
+    vertical-align: middle;
+`;
+
+//TODO: Hover behavior of button (see react-native-web-hover)
+
+const StyledPressable = styled.Pressable`
+  background-color: background;
+  border-radius: 100px;
+  line-height: 17px;
+  padding: 10px 30px;
+  vertical-align: middle;
+`
+
+const Pill = (props: Props) => {
+    const {title, onPress} = props;
+    return(
+        <StyledPressable onPress={onPress}>
+            <StyledText>
+                {title}
+            </StyledText>
+        </StyledPressable>
+    )
+}
+
+export default Pill;
---
+++ src/components/organisms/TypePills/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+// @ts-ignore
+import styled from "styled-components/native";
+import Pill from '../../atoms/Pill';
+import AllPill from '../../atoms/Pill';
+import PostMetadata from "../../atoms/Pill";
+
+
+interface Props {
+    TypeTitle1:string;
+    TypeTitle2:string;
+    TypeTitle3:string;
+    TypeTitle4:string;
+}
+
+const StyledView = styled.View`
+  background-color: #191932;
+  border-radius: 30px;
+  padding: 2px;
+  margin: 10px;
+`
+
+const StyledText = styled.Text`
+  color: white;
+  font-size: 10px;
+  padding: 10px 0px;
+`
+
+const TextPost = (props: Props) => {
+    const {TypeTitle1,  TypeTitle2, TypeTitle3, TypeTitle4} = props
+
+    return(
+        <StyledText>
+        <><StyledView><Pill title={TypeTitle1} onPress={function (): void { } }></Pill></StyledView>
+        <StyledView><Pill title={TypeTitle2} onPress={function (): void { } }></Pill></StyledView></>
+        <StyledView><Pill title={TypeTitle3} onPress={function (): void { } }></Pill></StyledView>
+        <StyledView><Pill title={TypeTitle4} onPress={function (): void { } }></Pill></StyledView>    
+        </StyledText>
+    )
+}
+
+TextPost.defaultProps = {
+    TypeTitle1: "All",
+    TypeTitle2: "Images",
+    TypeTitle3: "Videos",
+    TypeTitle4: "Text",
+    
+}
+
+export default TextPost;
