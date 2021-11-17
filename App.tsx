427683564b45bd58df87fe8b938d262043816dd5
Newman251 <79203167+Newman251@users.noreply.github.com>
2021-11-17 17:45:25 +0100

    Slight Cleanup

--- App.tsx
+++ App.tsx
@@ -1,15 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
-import MainButton from "./src/components/atoms/MainButton";
 import VideoPost from "./src/components/organisms/VIdeoPost";
 import {DividerShortRegular} from "fluent-icons-react";
 import TextPost from "./src/components/organisms/TextPost";
 import PhotoPost from "./src/components/organisms/PhotoPost";
-import InputField from "./src/components/molecules/InputField";
 import TypePills from "./src/components/organisms/TypePills";
-import KeywordPill from "./src/components/atoms/KeywordPill";
 import SearchBar from "./src/components/molecules/SearchBar";
+import PlatformPills from "./src/components/organisms/PlatformPills";
 // @ts-ignore
 import styled from "styled-components/native";
 import PostEngagement from "./src/components/molecules/PostEngagement";
@@ -30,6 +28,7 @@ export default function App() {
       <SearchBar/>
       <DividerShortRegular size={30} color="transparent"/>
       <TypePills/>
+      <PlatformPills/>
       <PostsContainer>
           <VideoPost/>
           <TextPost/>
