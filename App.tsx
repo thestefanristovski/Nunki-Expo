4a0ebe098d8993815272f4ce59ef21a0a10891fa
Newman251 <79203167+Newman251@users.noreply.github.com>
2021-11-17 13:17:38 +0100

    New features

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MainButton from "./src/components/atoms/MainButton";
 import VideoPost from "./src/components/organisms/VIdeoPost";
 import {DividerShortRegular} from "fluent-icons-react";
 import TextPost from "./src/components/organisms/TextPost";
 import PhotoPost from "./src/components/organisms/PhotoPost";
+import InputField from "./src/components/molecules/InputField";
+import TypePills from "./src/components/organisms/TypePills";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MainButton title="I'm a button"></MainButton>
-        <DividerShortRegular size={30} color="transparent"/>
+      <InputField onPress={function (): void {} }></InputField>
+      <DividerShortRegular size={30} color="transparent"/>
+      <TypePills/>
       <VideoPost/>
       <TextPost/>
       <PhotoPost/>
